258b39f4eae6ca3c69aaa93ad4b7edf48e8de033
jmanas <jmanas@dit.upm.es>
2017-06-28 20:24:31 +0200

    use CRT to decrypt
    smoke test cases

--- src/machine/RSASecretKey.java
+++ src/machine/RSASecretKey.java
@@ -2,8 +2,7 @@ package machine;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
-
-// 31.4.2012 check p = q.
+import java.util.Random;
 
 /**
  * RSA keys: secret part.
@@ -48,12 +47,15 @@ public class RSASecretKey {
                 q_1 = q.subtract(BigInteger.ONE);
                 gcd = e.gcd(q_1);
             } while (!gcd.equals(BigInteger.ONE));
-        } while (p.equals(q));
+
+            gcd = p.gcd(q);
+        } while (!gcd.equals(BigInteger.ONE));
 
         n = p.multiply(q);
+        publicKey = new RSAPublicKey(n, e);
+
         BigInteger phi = p_1.multiply(q_1);
         d = e.modInverse(phi);
-        publicKey = new RSAPublicKey(n, e);
     }
 
     public RSASecretKey(BigInteger n, BigInteger e, BigInteger d) {
@@ -90,7 +92,37 @@ public class RSASecretKey {
     }
 
     public BigInteger decrypt(BigInteger enc) {
+        if (size() < 1000)
+            return decrypt_raw(enc);
+        else
+            return decrypt_crt(enc);
+    }
+
+    private BigInteger decrypt_raw(BigInteger enc) {
         return enc.modPow(d, getN());
+    }
+
+    // CRT _ Chinese Remainder Theorem
+    // used in smart cards
+    public BigInteger decrypt_crt(BigInteger enc) {
+        // precalculations - when d is set
+        BigInteger n = getN();
+
+        BigInteger p_1 = p.subtract(BigInteger.ONE);
+        BigInteger q_1 = q.subtract(BigInteger.ONE);
+        BigInteger d_p_1 = d.mod(p_1);
+        BigInteger d_q_1 = d.mod(q_1);
+
+        BigInteger xp = q.multiply(q.modInverse(p));
+        BigInteger xq = p.multiply(p.modInverse(q));
+
+        // on black message
+        BigInteger mp = enc.modPow(d_p_1, p);
+        BigInteger mq = enc.modPow(d_q_1, q);
+        BigInteger sp = mp.multiply(xp);
+        BigInteger sq = mq.multiply(xq);
+        BigInteger m = sp.add(sq).mod(n);
+        return m;
     }
 
     public BigInteger sign(BigInteger msg) {
@@ -108,4 +140,28 @@ public class RSASecretKey {
         buffer.append("d= ").append(d.toString(16)).append('\n');
         return buffer.toString();
     }
+
+    public static void main(String[] args) {
+        test(256);
+        test(512);
+        test(1024);
+        test(2048);
+    }
+
+    private static Random random = new Random();
+
+    private static void test(int keyLength) {
+        for (int i= 0; i < 16; i++) {
+            RSASecretKey secretKey = new RSASecretKey(keyLength);
+            BigInteger red = BigInteger.valueOf(Math.abs(random.nextLong()));
+            BigInteger black = secretKey.encrypt(red);
+            BigInteger recovered1 = secretKey.decrypt(black);
+            BigInteger recovered2 = secretKey.decrypt_crt(black);
+            if (!red.equals(recovered1))
+                System.err.println("ERROR 1: " + keyLength);
+            if (!red.equals(recovered2))
+                System.err.println("ERROR 2: " + keyLength);
+        }
+    }
+
 }
